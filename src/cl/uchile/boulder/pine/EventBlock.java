89306ee26b1f1ee8963060b01b55f826aba85ed2
Sergio <elxsergio@gmail.com>
2016-06-11 21:38:01 -0400

    New Theme and icons, change gravity of blocks

--- src/cl/uchile/boulder/pine/EventBlock.java
+++ src/cl/uchile/boulder/pine/EventBlock.java
@@ -2,6 +2,7 @@ package cl.uchile.boulder.pine;
 
 
 import android.content.Context;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,14 +21,16 @@ public class EventBlock {
         duration = dur;
     }
 
-    public View getBlockView(){
+    public View createBlockView(){
         TextView v = new TextView(context);
         v.setText(name);
         v.setWidth(DefaultDimensions.BLOCK_WIDTH);
-        v.setX(DefaultDimensions.MARGIN_NUMBERS + (DefaultDimensions.BLOCK_WIDTH+2)*(day-1));
+        v.setX(DefaultDimensions.MARGIN_NUMBERS + (DefaultDimensions.BLOCK_WIDTH+2)*(day));
         v.setY(DefaultDimensions.BLOCK_HEIGHT_FACTOR*(minStart-60*6));
         v.setHeight((int) (DefaultDimensions.BLOCK_HEIGHT_FACTOR*duration));
         v.setBackgroundColor(0xff00cc00);
+        v.setTextColor(0xff000000);
+        v.setGravity(Gravity.CENTER);
         return v;
     }
 
--- src/cl/uchile/boulder/pine/MyActivity.java
+++ src/cl/uchile/boulder/pine/MyActivity.java
@@ -10,24 +10,28 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+
 public class MyActivity extends Activity {
 
     private RelativeLayout blockLayout;
     private boolean built;
     private EventosDB eventosDB;
+    private ArrayList<View> blocks;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         blockLayout = (RelativeLayout) findViewById(R.id.main_layout);
+        blocks = new ArrayList<>();
         built = false;
         blockLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
                 if(!built){
                     DefaultDimensions.BLOCK_HEIGHT_FACTOR = v.getHeight()/(60f*18);
-                    DefaultDimensions.MARGIN_NUMBERS = findViewById(R.id.hours_label).getWidth();
+                    DefaultDimensions.MARGIN_NUMBERS = findViewById(R.id.hours_label).getWidth() + 4;
                     DefaultDimensions.BLOCK_WIDTH = (v.getWidth() - DefaultDimensions.MARGIN_NUMBERS - 12)/7;
                     buildEventBlocks();
                     built = true;
@@ -37,8 +41,16 @@ public class MyActivity extends Activity {
        eventosDB = new EventosDB(this,"DBPine",null,2);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        buildEventBlocks();
+    }
+
     private void buildEventBlocks() {
-        blockLayout.removeAllViews();
+
+        for(View v : blocks)
+            blockLayout.removeView(v);
 
         SQLiteDatabase db = eventosDB.getReadableDatabase();
         Cursor cursor = db.rawQuery("select nom, descr, day, minstart, duration from fix_events",null);
@@ -51,7 +63,9 @@ public class MyActivity extends Activity {
                 int minstart = cursor.getInt(3);
                 int dur = cursor.getInt(4);
                 EventBlock block = new EventBlock(this,nom,day,minstart,dur);
-                blockLayout.addView(block.getBlockView());
+                View v = block.createBlockView();
+                blockLayout.addView(v);
+                blocks.add(v);
             }
             while(cursor.moveToNext());
         }
@@ -68,14 +82,14 @@ public class MyActivity extends Activity {
     @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         switch (item.getItemId()){
-            case R.id.menu_add:
-                gotoAddEventActivity();
+            case R.id.menu_add_fix:
+                gotoAddFixEventActivity();
                 break;
         }
         return true;
     }
 
-    private void gotoAddEventActivity() {
+    private void gotoAddFixEventActivity() {
         Intent i = new Intent(getBaseContext(),FixEventActivity.class);
         startActivity(i);
     }
